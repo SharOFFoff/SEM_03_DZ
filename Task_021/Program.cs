94a1486030fd760574c51b7e77d381334db0a128
SharOFF <sharov-pa@yandex.ru>
2022-12-01 00:37:07 +0300

    Task_021

---
+++ Task_021/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task_021
+Console.Clear();
+
+Console.Write("Введите координаты точки А:\n");
+double x1 = Convert.ToDouble(Console.ReadLine());
+double y1 = Convert.ToDouble(Console.ReadLine());
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координаты точки B:\n");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y2 = Convert.ToDouble(Console.ReadLine());
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine($"Расстояние между точками:{Math.Round(result,2)}");
