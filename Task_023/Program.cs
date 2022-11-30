870cc71a73cfb1cce1e51815773358904475cc6e
SharOFF <sharov-pa@yandex.ru>
2022-12-01 00:49:24 +0300

    Task_023

---
+++ Task_023/Program.cs
@@ -0,0 +1,13 @@
+﻿// Task_023
+Console.Clear();
+
+Console.Write("Введите число: ");
+double n = Convert.ToDouble(Console.ReadLine());
+
+double i = 1;
+while (i <= n)
+{
+    double m = Math.Pow(i , 3);
+    Console.Write($"{m} ");
+    i++;
+}
